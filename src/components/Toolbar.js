a2e637a1086552a6cb51abc14555155e49eb4b55
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-05-11 16:04:21 +0000

    contacts added.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { MenuItem, Select } from "@material-ui/core";
+
+import {
+  MenuItem,
+  Select,
+  IconButton,
+  Link,
+  Avatar,
+  Typography,
+  Toolbar,
+  AppBar,
+} from "@material-ui/core";
+import google from "../assets/google.svg";
+import twitter from "../assets/twitter.svg";
+import linkedin from "../assets/linkedin.svg";
+import github from "../assets/github.svg";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -25,6 +36,51 @@ export default function ToolbarTop({ onCountryChange, country, countries }) {
           <Typography variant="h6" className={classes.title}>
             Jina Covid Tracker
           </Typography>
+          {/* contacts */}
+          <Link
+            target="blank_"
+            color="inherit"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=jinacoolprojects@gmail.com">
+            <IconButton color="inherit">
+              <Avatar
+                style={{ width: "1.5em", height: "1.5em" }}
+                src={google}
+              />
+            </IconButton>
+          </Link>
+          <Link
+            target="blank_"
+            color="inherit"
+            href="https://www.linkedin.com/in/bargady-ahmed-082b30177">
+            <IconButton color="inherit">
+              <Avatar
+                style={{ width: "1.5em", height: "1.5em" }}
+                src={linkedin}
+              />
+            </IconButton>
+          </Link>
+          <Link
+            target="blank_"
+            color="inherit"
+            href="https://twitter.com/AhmedBargady?s=09">
+            <IconButton color="inherit">
+              <Avatar
+                style={{ width: "1.5em", height: "1.5em" }}
+                src={twitter}
+              />
+            </IconButton>
+          </Link>
+          <Link
+            target="blank_"
+            color="inherit"
+            href="https://github.com/AhmedCoolProjects">
+            <IconButton color="inherit">
+              <Avatar
+                style={{ width: "1.5em", height: "1.5em" }}
+                src={github}
+              />
+            </IconButton>
+          </Link>
           {/* dropdown countries */}
           <Select onChange={onCountryChange} value={country} variant="outlined">
             <MenuItem value="worldwide">Worldwide</MenuItem>
